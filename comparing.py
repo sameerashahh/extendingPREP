8fc7de8525387a3ab2a843dd43e2433f3d7f4d41
Sameera Shah <149904314+sameerashahh@users.noreply.github.com>
2025-10-03 01:16:46 -0700

    added checked files

--- comparing.py
+++ comparing.py
@@ -1,10 +1,9 @@
-# compare_files.py
 with open("prepfiles.txt") as f1, open("original.txt") as f2:
     names1 = set(line.strip() for line in f1)
     names2 = set(line.strip() for line in f2)
 
-only_in_file2 = names2 - names1
+prep_transforms_missing = names2-names1
 
 with open("differences.txt", "w") as out:    
-    for name in sorted(only_in_file2):
+    for name in sorted(prep_transforms_missing):
         out.write(name + "\n")
